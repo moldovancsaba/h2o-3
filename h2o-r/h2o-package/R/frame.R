4b4713c0cb670f2c17e5f4af8b02097fcfcee9e7
wendycwong <wendycwong@yahoo.com>
2017-07-07 15:14:15 -0700

    PUBDEV-4630: as.h2o.slow.  Incorporate MichalK code review suggestions.

--- h2o-r/h2o-package/R/frame.R
+++ h2o-r/h2o-package/R/frame.R
@@ -3229,13 +3229,35 @@ as.h2o.Matrix <- function(x, destination_frame="", ...) {
   if ( destination_frame=="" ) # .h2o.readSVMLight wont handle ""
     destination_frame <- .key.make("Matrix") # only used if `x` variable name not valid key
 
-  drs = as.simple_triplet_matrix(x)# need to convert sparse matrix x to a simple triplet matrix format
-  thefile <- tempfile()
-  .h2o.write_stm_svm(drs, file = thefile)
-  h2f <- h2o.uploadFile(thefile, parse_type = "SVMLight")
-  unlink(thefile)
-  h2f[- c(1)]# remove the first column
+  if (use.package("data.table",use=getOption("h2o.use.data.table", TRUE)) && use.package("slam", version="0.1.40", TRUE)) {
+    drs <- as.simple_triplet_matrix(x)# need to convert sparse matrix x to a simple triplet matrix format
+    thefile <- tempfile()
+    .h2o.write_stm_svm(drs, file = thefile)
+    h2f <<- h2o.uploadFile(thefile, parse_type = "SVMLight", destination_frame=destination_frame)
+    unlink(thefile)
+    h2f[, -1]   # remove the first column
+  } else {
+    warning("as.h2o can be slow for large sparse matrices.  Install packages data.table and slam to speed up as.h2o.")
+    tmpf <- tempfile(fileext = ".svm")
+    .h2o.write.matrix.svmlight(x, file = tmpf)
+    h2f <- .h2o.readSVMLight(tmpf, destination_frame = destination_frame)
+    file.remove(tmpf)
+    h2f
+  }
+}
 
+.h2o.write.matrix.svmlight <- function(matrix, file) {
+  on.exit(sink())
+  sink(file)
+  sapply(1:nrow(matrix), function(i) {
+    r <- matrix[i, ]
+    val.indices <- which(r != 0)
+    val.indices <- val.indices[val.indices > 1]
+    target <- r[1]
+    features <- paste(sprintf("%d", val.indices - 1), r[val.indices], collapse = " ", sep = ":")
+    line <- sprintf("%s %s\n", target, features)
+    cat(line)
+  })
 }
 
 # Convert a simple triplet matrix to svm format
@@ -3250,7 +3272,6 @@ as.h2o.Matrix <- function(x, destination_frame="", ...) {
     stop("y should be a vector of length equal to number of rows of stm")
   }
   n <- length(y)
-
 
   # data.table solution thanks to roland
   rowLeft <- setdiff(c(1:n), unique(stm$i))  # added two liner to return rows of zeros instead of repeating other rows
